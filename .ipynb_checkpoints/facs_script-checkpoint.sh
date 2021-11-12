2604a85d9fe0f17a9aefe26adb659f16cfb6939d
Arindam Saha <arindamsaha1507@gmail.com>
2021-11-12 15:10:25 +0000

    Initial commit

---
+++ .ipynb_checkpoints/facs_script-checkpoint.sh
@@ -0,0 +1,31 @@
+facs_dir='/home/arindam/Dropbox/Brunel/FACS/facs'
+cur_dir=$PWD
+
+runfile='run.py'
+
+transition_scenario=$3
+transition_mode=1
+output_dir="results"
+location=$1
+simulation_period=$2
+
+cmd="python3 $runfile --location=$location --transition_scenario=$transition_scenario --simulation_period=$simulation_period --output_dir=$output_dir"
+
+suffix='_buildings.csv'
+
+cp $cur_dir/Trial_Data/age-distr.csv $facs_dir/covid_data/
+cp $cur_dir/Trial_Data/$location$suffix $facs_dir/covid_data/
+cd $facs_dir
+
+$cmd | tee log.txt
+
+datestamp=`date '+%F_%H:%M:%S'` 
+
+suffix='--1.csv'
+joiner='-'
+
+cp $facs_dir/$output_dir/$location$joiner$transition_scenario$datestamp$suffix $cur_dir/Results/
+
+suffix1='-latest.csv'
+
+cp $facs_dir/$output_dir/$location$joiner$transition_scenario$datestamp$suffix $cur_dir/Results/$location$suffix1
